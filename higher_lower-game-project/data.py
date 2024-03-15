98676359d648fbcde22480db3c22c274f687f895
ankitsensei <main.ankitbhagat@gmail.com>
2024-03-15 21:00:38 +0530

    created randomness in keys of the dict

---
+++ higher_lower-game-project/data.py
@@ -0,0 +1,10 @@
+informations = {
+    "youtube":137500000,
+    "amazon":104400000,
+    "facebook":98140000,
+    "google":58570000,
+    "wordle":58740000,
+    "weather":	57810000,
+    "gmail":52000000,
+    "google translate":39270000
+    }
--- higher_lower-game-project/main.py
+++ higher_lower-game-project/main.py
@@ -1,6 +1,10 @@
 import os
 import art
+import data
+import random
 
+print(art.higher_lower)
+print(art.vs)
 
 def clear_screen():
     if os.name == 'nt':
@@ -11,3 +15,23 @@ def clear_screen():
 # clear_screen()
 
 
+
+
+def random_key(info):
+    keys = info.keys()
+    key = random.choice(list(info.keys()))
+    return key
+
+
+
+info = data.informations
+while True:
+    bot = random_key(info)
+    user = random_key(info)
+    while user == bot:
+        user = random_key(info)
+    print("A.",bot)
+    print("B.",user)
+
+    ask = input("Which one is more searched? [A/B]: ").upper()
+    values_of_keys(info, bot, user)
