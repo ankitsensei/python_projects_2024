e0015ea3efb45e658a287cbe41c37d7ecb4cbe67
ankitsensei <main.ankitbhagat@gmail.com>
2024-06-02 21:57:07 +0530

    LEETCODE... reverse_string.. 2nd method

--- #leetcode/easy/reverse_string.py
+++ #leetcode/easy/reverse_string.py
@@ -1,14 +1,32 @@
+# class Solution:
+#     def reverseString(self, s: list[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#         y = []
+#         for i in s:
+#             y.append(i)
+#         s = y[::-1]
+#         print(s)
+
+# s = "hello"
+# sol1 = Solution()
+# sol1.reverseString(s)
+
+# 2nd Method-------------------------------------------------------
 class Solution:
     def reverseString(self, s: list[str]) -> None:
         """
-        Do not return anything, modify s in-place instead.
+        Reverse the input list of characters in place.
         """
-        y = []
-        for i in s:
-            y.append(i)
-        s = y[::-1]
-        print(s)
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
 
-s = "hello"
-sol1 = Solution()
-sol1.reverseString(s)
+# Example usage
+s = ['a', 'n', 'k', 'i', 't']
+sol = Solution()
+sol.reverseString(s)
+print(s)  # Output should be ['t', 'i', 'k', 'n', 'a']
